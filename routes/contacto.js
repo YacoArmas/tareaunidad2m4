32580ec09c00f227a3a6d5777c27056c2db210a4
YacoArmas <armasyaco@gmail.com>
2021-09-22 11:54:01 -0300

    first commit

--- routes/contacto.js
+++ routes/contacto.js
@@ -1,9 +1,48 @@
 var express = require("express");
 var router = express.Router();
+var nodemailer = require("nodemailer");
 
 /* GET home page. */
 router.get("/", function (req, res, next) {
-  res.render("contacto");
+  res.render("contacto", {
+    isContacto: true,
+  });
+});
+
+router.post("/", async function (req, res, next) {
+  var nombre = req.body.nombre;
+  var email = req.body.email;
+  var telefono = req.body.tel;
+  var mensaje = req.body.mensaje;
+
+  var obj = {
+    to: "armasyaco@gmail.com",
+    subject: "Contacto desde la web Eramos Tan Jovenes",
+    html:
+      nombre +
+      " Se contacto y quiere mas informacion a este correo: " +
+      email +
+      " Ademas, hizo el siguiente comentario: " +
+      mensaje +
+      ". Su telefono es: " +
+      telefono,
+  };
+
+  var transport = nodemailer.createTransport({
+    host: "smtp.mailtrap.io",
+    port: 2525,
+    auth: {
+      user: "38ad656675baef",
+      pass: "606119055fb531",
+    },
+  });
+
+  var info = await transport.sendMail(obj);
+
+  res.render("contacto", {
+    isContacto: true,
+    message: "Mensaje enviado correctamente",
+  });
 });
 
 module.exports = router;
